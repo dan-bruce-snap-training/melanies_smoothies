23c7fc5e4b9a46f2fb5e5f3f66e51b087d25650f
dan-bruce-snap-training <169158770+dan-bruce-snap-training@users.noreply.github.com>
2024-05-07 14:11:59 +0100

    Update streamlit_app.py
    pd stuff

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,12 @@
 # Import python packages
 import streamlit as st
 import requests
+import pandas as pd
 from snowflake.snowpark.functions import col
 
+
 # api initiation
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+
 
 # Write directly to the app
 st.title(":cup_with_straw: Customise Your Smoothie! :cup_with_straw:")
@@ -16,8 +17,11 @@ st.write("The name on your Smoothie will be:", name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
+
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
 
 ingredient_list = st.multiselect(
     "Choose up to 5 ingredients:",
@@ -30,9 +34,14 @@ if ingredient_list:
 
     ingredients_string = ''
     for fruit_chosen in ingredient_list:
-        ingredients_string += ' '+fruit_chosen
+        ingredients_string += fruit_chosen + ' '
 
-    #st.write(ingredients_string)
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
             values ('""" + ingredients_string + """','""" + name_on_order + """')"""
